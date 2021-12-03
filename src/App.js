82e5b23dfd66a2ed5191e414c89cffd26777b464
Jonathan Montes Romero <jmontes8686@gmail.com>
2021-12-02 22:05:41 -0800

    basic Header added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import Header from './components/Header';
+import Navbar from './components/Navbar'
+// import {Main} from './components/Main';
+// import {Footer} from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      <Header />
+      {/* <Main />
+      <Footer/> */}
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+   <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+  <a className="navbar-brand" href="#">Navbar</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul className="navbar-nav ml-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Home </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Skills</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Work</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Contact Me</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+  )
+}
+
+export default Navbar
